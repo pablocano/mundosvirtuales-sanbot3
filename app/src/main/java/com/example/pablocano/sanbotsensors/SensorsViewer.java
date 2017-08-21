c34bb6712f888cdcbf5df256fc16e4bdc5c2e1cd
Pablo Cano <pablo.cano@mundos-virtuales.com>
2017-08-21 18:02:04 -0300

    New app to test the ir sensors

--- app/src/main/java/com/example/pablocano/sanbotsensors/SensorsViewer.java
+++ app/src/main/java/com/example/pablocano/sanbotsensors/SensorsViewer.java
@@ -1,8 +1,18 @@
 package com.example.pablocano.sanbotsensors;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.TextView;
 
 import com.qihancloud.opensdk.base.TopBaseActivity;
 import com.qihancloud.opensdk.beans.FuncConstant;
@@ -14,11 +24,20 @@ public class SensorsViewer extends TopBaseActivity {
     GridView sensorsGrid;
     HardWareManager hardWareManager;
     SensorItemAdapter sensorItemAdapter;
+    Button button;
+    EditText sensorSelector;
+
+    int selectedSensor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensors_viewer);
+
+        selectedSensor = 0;
+
+        sensorSelector = (EditText) findViewById(R.id.selected_sensor);
+        button = (Button) findViewById(R.id.select_button);
 
         sensorsGrid = (GridView) findViewById(R.id.sensorsGridView);
         sensorItemAdapter = new SensorItemAdapter(getApplicationContext());
@@ -33,6 +52,55 @@ public class SensorsViewer extends TopBaseActivity {
 
     @Override
     protected void onMainServiceConnected(){
+        button.setEnabled(false);
+
+        sensorSelector.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = true;
+                if (actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+                    try {
+                        int value = Integer.parseInt(String.valueOf(v.getText()));
+                        if (value > 0 && value < 18) {
+                            InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                            in.hideSoftInputFromWindow(v.getApplicationWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+                            selectedSensor = value;
+                            handled = false;
+                        }
+                    } catch (NumberFormatException e) {
+
+                    }
+                }
+                return handled;
+            }
+        });
+
+        sensorSelector.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try{
+                    int value = Integer.parseInt(s.toString());
+                    if (value > 0 && value < 18) {
+                        button.setEnabled(true);
+                    }
+                    else{
+                        button.setEnabled(false);
+                    }
+                } catch (NumberFormatException e) {
+                    button.setEnabled(false);
+                }
+            }
+        });
     }
 
     private void initSensorListeners(){
@@ -43,5 +111,18 @@ public class SensorsViewer extends TopBaseActivity {
                 sensorItemAdapter.notifyDataSetChanged();
             }
         });
+    }
+
+    public void selectSensor(View view){
+        try {
+            int value = Integer.parseInt(String.valueOf(sensorSelector.getText()));
+            if (value > 0 && value < 18) {
+                InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                in.hideSoftInputFromWindow(sensorSelector.getApplicationWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+                selectedSensor = value;
+            }
+        } catch (NumberFormatException e) {
+
+        }
     }
 }
